358baf2824bf4229b733fad2a6bde06fca149d03
R. Tony Goold <tony@wattpad.com>
2014-01-27 11:26:37 -0500

    Initial Commit

---
+++ ObjClang/ObjClang.h
@@ -0,0 +1,13 @@
+//
+//  ObjClang.h
+//  ObjClang
+//
+//  Created by R. Tony Goold on 2014-01-27.
+//  Copyright (c) 2014 R. Tony Goold. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjClang : NSObject
+
+@end
